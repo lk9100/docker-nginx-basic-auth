8dd4284447d56af25c80db4564f05796c7818522
Kai Lan <1432536+lk9100@users.noreply.github.com>
2024-01-03 14:57:25 +0000

    add specific var names for envsubst so that it does not mess with nginx config

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm /etc/nginx/conf.d/default.conf || :
-envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
+envsubst '${FORWARD_HOST},${FORWARD_PORT}' < auth.conf > /etc/nginx/conf.d/auth.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
 exec nginx -g "daemon off;"
